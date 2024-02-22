9ae45b28a7d72d6dfa426771c3893cbbcfd9db6d
Egwu-Marvin <egwumarvin@gmail.com>
2024-02-22 13:22:48 +0100

    feat:project

--- src/app/products/accessories/page.jsx
+++ src/app/products/accessories/page.jsx
@@ -8,24 +8,14 @@ export default function accessories() {
       
         <ProductImage src='/img/powerBox2.jpg' alt='laptop powerbank' pname='Laptop PowerBox' />
         
-
-      
         <ProductImage src='/img/powerbank2.jpg' alt='powerbank' pname='Power Bank' />
         
-
-      
         <ProductImage src='/img/headphoneStanding.jpg' alt='Wired headphone' pname='Wired Headphone' />
         
-
-      
         <ProductImage src='/img/headphone.jpg' alt='Wireless headphone' pname='Wireless Headphone' />
         
-
-      
         <ProductImage src='/img/earphone.jpg' alt='earbud' pname='Earbud' />
         
-
-      
         <ProductImage src='/img/laptopCharger.jpg' alt='laptop charger' pname='Laptop Charger' />
         
     </div>
